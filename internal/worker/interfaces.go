00004a2aa4d6b79f72f0010001b821de049a18be
Marco Primi <marco@mpri.me>
2023-04-04 14:26:14 -0700

    Remove dead code

--- internal/worker/interfaces.go
+++ internal/worker/interfaces.go
@@ -2,6 +2,7 @@ package worker
 
 import (
 	"context"
+
 	"github.com/mprimi/go-bench-away/pkg/core"
 )
 
@@ -22,26 +23,3 @@ type WorkerClient interface {
 	DispatcherClient
 	JobUpdaterClient
 }
-
-// type OmniClient interface {
-// 	Close()
-// 	CreateJobsQueue() error
-// 	CreateJobsRepository() error
-// 	CreateArtifactsStore() error
-// 	DeleteJobsQueue() error
-// 	DeleteJobsRepository() error
-// 	DeleteArtifactsStore() error
-// 	DispatchJobs(context.Context, HandleJobFunc) error
-// 	LoadJob(jobId string) (*core.JobRecord, uint64, error)
-// 	LoadRecentJobs(int) ([]*core.JobRecord, error)
-// 	SubmitJob(params core.JobParameters) (*core.JobRecord, error)
-// 	UpdateJob(*core.JobRecord, uint64) (uint64, error)
-// 	UploadLogArtifact(string, string) (string, error)
-// 	UploadResultsArtifact(string, string) (string, error)
-// 	UploadScriptArtifact(string, string) (string, error)
-// 	DownloadLogArtifact(*core.JobRecord, string) error
-// 	DownloadResultsArtifact(*core.JobRecord, string) error
-// 	DownloadScriptArtifact(*core.JobRecord, string) error
-// 	LoadResultsArtifact(*core.JobRecord) ([]byte, error)
-// 	LoadLogArtifact(*core.JobRecord) ([]byte, error)
-// }
